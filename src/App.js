7464ad595a0417b23847d296e93ac93dfde0f221
akhil <akhil.ce08@gmail.com>
2024-01-13 17:22:31 +0530

    first commit and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+  const[num1, setNum1]= useState("");
+  const[num2, setNum2]= useState("");
+  const[result, setResult]= useState("");
+
+  function implementAdd(){
+    const finaladdResult= parseInt(num1) + parseInt(num2);
+    setResult(finaladdResult);
+    setNum1("");
+    setNum2("");
+  }
+
+  function implementSub(){
+    const finalsubResult= parseInt(num1) - parseInt(num2);
+    setResult(finalsubResult);
+    setNum1("");
+    setNum2("");
+  }
+
+  function implementMul(){
+    const finalmulResult= parseInt(num1) * parseInt(num2);
+    setResult(finalmulResult);
+    setNum1("");
+    setNum2("");
+
+  }
+
+  function implementDiv(){
+    const finaldivResult= parseInt(num1) / parseInt(num2);
+    setResult(finaldivResult);
+    setNum1("");
+    setNum2("");
+
+  }
+  return(
+    <div className="app">
+      <div className="calculator">
+        <h3 className="heading">React Calculator</h3>
+        <input type="text" placeholder="Num1" value={num1} onChange={(e)=>setNum1(e.target.value)}></input><br/>
+        <input type="text" placeholder="Num2" value={num2} onChange={(e)=>setNum2(e.target.value)}></input><br/>
+        <button onClick={implementAdd}>+</button>
+        <button onClick={implementSub}>-</button>
+        <button onClick={implementMul}>*</button>
+        <button onClick={implementDiv}>/</button>
+
+        {
+          result && (
+            <div className="result">
+              <span>Result - </span>{result}
+            </div>
+          )
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
